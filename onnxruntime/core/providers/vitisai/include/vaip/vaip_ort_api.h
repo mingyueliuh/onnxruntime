24e663911061a93dafbaf3bb7b76f47ede618cbe
mingyue <mingyue@amd.com>
2024-09-23 23:25:13 -0400

    add get_patterrn_list API and change get_pattern API

--- onnxruntime/core/providers/vitisai/include/vaip/vaip_ort_api.h
+++ onnxruntime/core/providers/vitisai/include/vaip/vaip_ort_api.h
@@ -234,9 +234,10 @@ struct OrtApiForVaip {
                                 size_t input_xmodel_size,
                                 const char* config_xmodel,
                                 size_t config_xmodel_size, void* state,
-                                char* (*allocator)(void*, size_t));                                      // [94]
-  const char* (*vaip_get_default_config)();                                                              // [95]
-  bool (*vaip_get_pattern_as_binary)(const char* name, void* state, char* (*allocator)(void*, size_t));  // [96]
+                                char* (*allocator)(void*, size_t));  // [94]
+  const char* (*vaip_get_default_config)();                         // [95]
+  int (*vaip_get_pattern_as_binary)(const char* name, void* state, void (*k)(void*, void*, size_t));  // [96]
+  void (*vaip_get_pattern_list)(void* state, void (*k)(void*, void*, size_t));  // [97]
 };
 
 #ifndef USE_VITISAI
